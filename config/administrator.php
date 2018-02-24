99594475f03e54412efc7615dd169279005c1fc0
AGOBW-801081652\Administrator <zhoufubing@5ifangxin.com>
2018-02-24 09:43:04 +0800

    '分类话题回复后台管理'

--- config/administrator.php
+++ config/administrator.php
@@ -39,6 +39,13 @@ return array(
     'menu' => [
         '用户与权限' => [
             'users',
+            'roles',
+            'permissions',
+        ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
         ],
     ],
 
