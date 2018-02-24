f1ee4f864410b963c0e476665314a185110b9edf
AGOBW-801081652\Administrator <zhoufubing@5ifangxin.com>
2018-02-24 10:39:59 +0800

    '活跃用户'

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
@@ -17,11 +18,12 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic)
-	{
-	    $topics = $topic->withOrder($request->order)->paginate(20);
-		return view('topics.index', compact('topics'));
-	}
+    public function index(Request $request, Topic $topic, User $user)
+    {
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics', 'active_users'));
+    }
 
     public function show(Request $request,Topic $topic)
     {
