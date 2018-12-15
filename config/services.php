63f7500ef67ad25b23143c588d8a920b8bb10dd9
zhoufubing <zhoufubing@5ifangxin.com>
2018-12-15 10:30:41 +0800

    '微信登陆'

--- config/services.php
+++ config/services.php
@@ -39,5 +39,9 @@ return [
         'key'   => env('BAIDU_TRANSLATE_KEY'),
     ],
 
-
+    'weixin' => [
+        'client_id' => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect' => env('WEIXIN_REDIRECT_URI'),
+    ]
 ];
