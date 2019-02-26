3bb62309083930a7a5ac53d0ea22600ce602f3e2
zhoufubing <zhoufubing@5ifangxin.com>
2019-02-26 15:50:37 +0800

    刷新token

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -11,16 +11,17 @@ class AuthorizationsController extends Controller
 {
     public function socialStore($type, SocialAuthorizationRequest $request)
     {
-        if (!in_array($type,['weixin'])){
+        if (!in_array($type, ['weixin'])) {
             return $this->response->errorBadRequest();
         }
 
         $driver = \Socialite::driver($type);
-        try{
-            if ($code = $request->code){
+
+        try {
+            if ($code = $request->code) {
                 $response = $driver->getAccessTokenResponse($code);
                 $token = array_get($response, 'access_token');
-            }else{
+            } else {
                 $token = $request->access_token;
 
                 if ($type == 'weixin') {
@@ -29,11 +30,11 @@ class AuthorizationsController extends Controller
             }
 
             $oauthUser = $driver->userFromToken($token);
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             return $this->response->errorUnauthorized('参数错误，未获取用户信息');
         }
 
-        switch ($type){
+        switch ($type) {
             case 'weixin':
                 $unionid = $oauthUser->offsetExists('unionid') ? $oauthUser->offsetGet('unionid') : null;
 
@@ -43,18 +44,23 @@ class AuthorizationsController extends Controller
                     $user = User::where('weixin_openid', $oauthUser->getId())->first();
                 }
 
-                if (!$user){
+                // 没有用户，默认创建一个用户
+                if (!$user) {
                     $user = User::create([
                         'name' => $oauthUser->getNickname(),
                         'avatar' => $oauthUser->getAvatar(),
                         'weixin_openid' => $oauthUser->getId(),
-                        'weixin_unionid' => $unionid,
+                        'weixin_unionid' => $oauthUser->offsetGet('unionid'),
                     ]);
                 }
-            break;
+
+                break;
         }
-        return $this->response->array(['token' => $user->id]);
+
+        $token = Auth::guard('api')->fromUser($user);
+        return $this->respondWithToken($token)->setStatusCode(201);
     }
+
 
     public function store(AuthorizationRequest $request)
     {
@@ -70,10 +76,27 @@ class AuthorizationsController extends Controller
             return $this->response->errorUnauthorized('用户名或密码错误');
         }
 
+        return $this->responseWithToken($token)->setStatusCode(201);
+    }
+
+    public function responseWithToken($token)
+    {
         return $this->response->array([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
-        ])->setStatusCode(201);
+            'access_token'=>$token,
+            'token_type'=>'Bearer',
+            'expires_in'=>\Auth::guard('api')->factory()->getTTL() * 60,
+        ]);
+    }
+
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        Auth::guard('api')->logout();
+        return $this->response->noContent();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,12 @@ $api->version('v1',[
         // 登录
         $api->post('authorizations', 'AuthorizationsController@store')
             ->name('api.authorizations.store');
+        // 刷新token
+        $api->put('authorizations/current', 'AuthorizationsController@update')
+            ->name('api.authorizations.update');
+        // 删除token
+        $api->delete('authorizations/current', 'AuthorizationsController@destroy')
+            ->name('api.authorizations.destroy');
     });
 
 });
